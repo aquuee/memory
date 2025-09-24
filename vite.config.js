837abfcbadf30c17f8b56be7c4a1dce9b531a5a3
aque <roman.savin1802@gmail.com>
2025-09-24 11:08:15 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import inspect from 'vite-plugin-inspect'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+export default defineConfig({
+    base:"/memory/",
+plugins: [inspect(), ViteImageOptimizer({
+        jpg: {
+    quality: 60,
+}
+     }),],
+});
